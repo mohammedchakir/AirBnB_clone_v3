a5904d604b830f8bc657b914a333110d8b671347
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-22 14:19:52 +0100

    add and merge

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,26 @@
 """
 Defines routes for API v1 status endpoint
 """
-
-from api.v1.views import app_views
+from flask import Flask
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def get_status():
-    """
-    Returns status in JSON format
-    """
+    """Returns status in JSON format"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """Returns the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
