e568750f2390918c6090f50c212a2f8230b7ea79
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-22 13:58:25 +0100

    add

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines routes for API v1 status endpoint
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """
+    Returns status in JSON format
+    """
+    return jsonify({"status": "OK"})
