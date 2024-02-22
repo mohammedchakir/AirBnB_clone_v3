df61420314725a1ac49d193a032552236bf4dd93
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-22 23:40:19 +0100

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Main application file for API v1"""
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -19,7 +19,7 @@ def teardown_appcontext(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
