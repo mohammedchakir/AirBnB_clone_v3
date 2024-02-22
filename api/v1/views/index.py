5a120c52b9689928841a866bb63d02f1a216665d
mohammedchakir <zerroukimedchakir@gmail.com>
2024-02-22 16:36:37 +0100

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,10 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def get_status():
     """Returns status in JSON format"""
-    status={
-        "status": "OK"
-        }
-    return jsonify(status)
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
